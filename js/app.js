da10a59cb1e7c0dc0c9ab402441db9924721a827
Mohammadbilal90 <bilal.asif@centricdxb.com>
2025-06-20 14:00:43 +0500

    creating navbar responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-  AOS.init();
+AOS.init();
+
+
+
+
+
+
+  const menuToggle = document.getElementById('mobile-menu');
+  const navList = document.querySelector('.nav-list');
+
+  menuToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
+
+
