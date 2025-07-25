ea333582a5dcd1ef6866fbf93570cb96c253dc31
Mohammadbilal90 <bilal.asif@centricdxb.com>
2025-07-25 20:28:10 +0500

    some changes and push

--- js/app.js
+++ js/app.js
@@ -17,3 +17,54 @@ AOS.init();
   hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
   });
+
+
+let Number = [" 5, 10 , 15, 20 "];
+
+Number.forEach(function (num) {
+  console.log("Value:" + num);
+});
+
+
+let numbers = [5, 10, 15, 20];
+let i = 0;         
+let sum = 0;       
+
+while (i < numbers.length) {
+  sum = sum + numbers[i];  
+  i++;                      
+}
+
+console.log("Total Sum:", sum);
+
+let NumBers = [5, 10, 15, 20];
+NumBers.forEach(function (num) {
+  console.log(num * 2)
+});
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("Bilal");
+
+
+function add(m , s){
+  return m + s;
+}
+
+
+let result = add(95, 150);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
